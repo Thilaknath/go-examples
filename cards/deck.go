fdc37129baf233d0392a979f9027af6567e93418
Thilaknath Ashok Kumar <thilaknath.ashok.kumar@sap.com>
2019-07-17 08:53:46 -0400

    Add _ as variable replacement and newDeck nmethod

--- cards/deck.go
+++ cards/deck.go
@@ -7,6 +7,21 @@ import (
 // Create a new type of Deck, which is a slice of Strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string {"Spades" , "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string {"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardValues {
+			cards = append(cards, value+" of"+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
